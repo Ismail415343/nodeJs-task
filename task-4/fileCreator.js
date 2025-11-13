c0cea3d70bb27a4de81bd10c1b80eafdb51d28c0
Ismail415343 <muhammadmusa12689@gmail.com>
2025-11-13 11:01:07 +0500

    task 4

--- task-4/fileCreator.js
+++ task-4/fileCreator.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+/*const fs = require("fs");
 
 const args = process.argv.slice(2);
 const fileName = args[0];
@@ -12,5 +12,30 @@ const result = fs.readFile('contact.txt',"utf-8" ,(err, result) =>{
         return;
     }
     console.log(result);
-} );
+} );*/
 
+
+// task 4 
+
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+const fileName = args[0];
+const content = args.slice(1).join(' ');
+
+if(!fileName || !content){
+  console.log("Usage: node fileCreator.js <fileName> <content>");
+//   process.exit(1);    
+return;
+}
+
+fs.writeFileSync(fileName, content);
+console.log(`file ${fileName} created sucessfully`);
+
+const result = fs.readFileSync(fileName,"utf-8", (err, result)=>{
+    if(err){
+        console.log(err+" error occured");
+    } else{
+         console.log(`file contect ${content}`);
+    }
+});
