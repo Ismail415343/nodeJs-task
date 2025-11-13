cec1639aeeeabe95247eb0232e4f75215ff8de69
Ismail415343 <muhammadmusa12689@gmail.com>
2025-11-13 10:44:27 +0500

    practice

---
+++ task-4/fileCreator.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+const fileName = args[0];
+const content = args.slice(1).join(" ");
+
+fs.writeFileSync(" message.txt","hello aorld");
+
+const result = fs.readFile('contact.txt',"utf-8" ,(err, result) =>{
+    if (err){
+        console.log(err+ " error occured")
+        return;
+    }
+    console.log(result);
+} );
+
