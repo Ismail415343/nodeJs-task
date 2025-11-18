baa467424e8f75ec87bd165eb05fe17547b86ab4
Ismail415343 <muhammadmusa12689@gmail.com>
2025-11-19 02:14:19 +0500

    TASK-6

---
+++ task-6/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get("/welcome", (req,res)=>{
+    res.json({message: "welcome to your first api"})
+});
+
+
+app.get("/greet", (req,res)=>{
+    const name = req.query.name;
+if(name){
+res.json({message: `Hello ${name}`})
+return;
+}
+res.json({message: "hello guest"})
+
+})
+
+app.get("/student/:name", (req,res)=>{
+    const student = req.params.name;
+  res.json({
+    student,
+    status: 'Record fetched successfully'
+  });
+
+})
+
+app.listen(PORT, ()=>{
+console.log(`serverr running successfully on the port ${PORT}`)
+});
