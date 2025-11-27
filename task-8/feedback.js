fab2d5ab2cd1eaec7b643fb8351991583e9e4fef
Ismail415343 <muhammadmusa12689@gmail.com>
2025-11-27 09:27:03 +0500

    task 8

---
+++ task-8/feedback.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let feedbackList = [];
+
+app.post("/feedback", (req, res)=>{
+
+    const {name,message} = req.body;
+
+
+
+if(!name || !message){
+    return res.status(400).json({
+    status : "error",
+    message: "name and message required"
+        
+    
+    })
+}
+
+const newFeedback = { name, message};
+
+feedbackList.push(newFeedback);
+
+return res.json({
+    status: "success",
+    data : newFeedback
+})
+
+})
+
+app.get("/feedback", (req,res)=>{
+
+res.json({
+    feedback: feedbackList
+
+})
+
+})
+
+app.listen(3000, () => {
+  console.log("Feedback API running at http://localhost:3000");
+});
